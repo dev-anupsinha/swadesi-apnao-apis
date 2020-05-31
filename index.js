120f3720829a01397272b3c0b82aa81fa7f6df44
Anup Kumar Sinha <AnupS@skytechsolutions.com>
2020-05-31 12:39:50 +0530

    modified apis

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const bodyparser = require("body-parser");
 var connectionProvider = require("./config/mysqlConnectionStringProvider"); //connection path
 
 app.use(bodyparser.json());
-
+//process.env.PORT
 app.listen(process.env.PORT, () =>
-  console.log("Express server is runnig at port no : 3000")
+  console.log("Express server is runnig at port no :", process.env.PORT)
 );
 
 var connection = connectionProvider.mysqlConnectionStringProvider.getMySqlConnection();
 
 //Get all employees
-app.get("/employees", (req, res) => {
-  connection.query("SELECT * FROM tbl_employee", (err, rows, fields) => {
+app.get("/allProductList", (req, res) => {
+  connection.query("SELECT * FROM v_product_list", (err, rows, fields) => {
     if (!err) res.send(rows);
     else console.log(err);
   });
 });
 
 //Get an employees
-app.get("/employees/:id", (req, res) => {
+app.get("/getProductItemByState/:stateCode", (req, res) => {
   connection.query(
-    "SELECT * FROM Employee WHERE EmpID = ?",
-    [req.params.id],
+    "SELECT * FROM v_product_list WHERE state_code = ?",
+    [req.params.stateCode],
     (err, rows, fields) => {
       if (!err) res.send(rows);
       else console.log(err);
@@ -32,49 +32,49 @@ app.get("/employees/:id", (req, res) => {
 });
 
 //Delete an employees
-app.delete("/employees/:id", (req, res) => {
-  connection.query(
-    "DELETE FROM Employee WHERE EmpID = ?",
-    [req.params.id],
-    (err, rows, fields) => {
-      if (!err) res.send("Deleted successfully.");
-      else console.log(err);
-    }
-  );
-});
+// app.delete("/employees/:id", (req, res) => {
+//   connection.query(
+//     "DELETE FROM Employee WHERE EmpID = ?",
+//     [req.params.id],
+//     (err, rows, fields) => {
+//       if (!err) res.send("Deleted successfully.");
+//       else console.log(err);
+//     }
+//   );
+// });
 
 //Insert an employees
-app.post("/employees", (req, res) => {
-  let emp = req.body;
-  var sql =
-    "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-  connection.query(
-    sql,
-    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
-    (err, rows, fields) => {
-      if (!err)
-        rows.forEach((element) => {
-          if (element.constructor == Array)
-            res.send("Inserted employee id : " + element[0].EmpID);
-        });
-      else console.log(err);
-    }
-  );
-});
+// app.post("/employees", (req, res) => {
+//   let emp = req.body;
+//   var sql =
+//     "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+//     CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+//   connection.query(
+//     sql,
+//     [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+//     (err, rows, fields) => {
+//       if (!err)
+//         rows.forEach((element) => {
+//           if (element.constructor == Array)
+//             res.send("Inserted employee id : " + element[0].EmpID);
+//         });
+//       else console.log(err);
+//     }
+//   );
+// });
 
 //Update an employees
-app.put("/employees", (req, res) => {
-  let emp = req.body;
-  var sql =
-    "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-  connection.query(
-    sql,
-    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
-    (err, rows, fields) => {
-      if (!err) res.send("Updated successfully");
-      else console.log(err);
-    }
-  );
-});
+// app.put("/employees", (req, res) => {
+//   let emp = req.body;
+//   var sql =
+//     "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+//     CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+//   connection.query(
+//     sql,
+//     [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+//     (err, rows, fields) => {
+//       if (!err) res.send("Updated successfully");
+//       else console.log(err);
+//     }
+//   );
+// });
