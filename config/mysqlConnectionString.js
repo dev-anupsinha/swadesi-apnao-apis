2c619720e602dd0ad0ab31bb08f2727cca6380cf
Anup Kumar Sinha <AnupS@skytechsolutions.com>
2020-05-31 11:48:40 +0530

    database connection modified

--- config/mysqlConnectionString.js
+++ config/mysqlConnectionString.js
@@ -1,18 +1,18 @@
 var mysqlConnectionString = {
   connection: {
     dev: {
-      host: "localhost",
-      user: "root",
-      password: "P@ssword@12",
-      database: "testdb",
+      host: "den1.mysql3.gear.host",
+      user: "sbswadesiapnao",
+      password: "Cn4m9a!biiA~",
+      database: "sbswadesiapnao",
       multipleStatements: true,
     },
 
     qa: {
       host: "localhost",
       user: "root",
-      password: "",
-      database: "swadesi_projects",
+      password: "P@ssword@12",
+      database: "testdb",
     },
 
     prod: {
